d9481d2ff0bb7d60a3c47ecb742a6c461ce9ef59
Myles Liu <lyx981mike@gmail.com>
2021-02-12 22:24:20 +0000

    repo cleaned

--- json_module/example.py
+++ json_module/example.py
@@ -3,11 +3,13 @@ from restaurant import restaurant
 from order import order
 from confirmation import confirmation
 
+data_dir = "../data/"
+
 def run_eample_one():
     #example 1: simulate json passed as string:
     
     #Step 1) Load json file as dict:
-    example_restaurant_json_path = "../restaurant_json/resaurant_id_1_in_n_out.json"
+    example_restaurant_json_path = data_dir + "restaurant_json/resaurant_id_1_in_n_out.json"
     data_as_dict = load_json_file_as_dict(example_restaurant_json_path)
 
     #Step 2) *Optional* Check dict is correct structure/ has all necessary keys:
@@ -25,14 +27,14 @@ def run_eample_one():
     #Step 7) *Optional* Use this dict for processing
 
     #Step 8) Write dict of json to json file:
-    example_restraunt_json_output_path = "../restaurant_json/sample_output.json"
+    example_restraunt_json_output_path = data_dir + "restaurant_json/sample_output.json"
     write_dict_to_json_file(data_recieved_as_dict, example_restraunt_json_output_path)
 
 def run_eample_two():
     #example 2: load json file of restraunt as restraunt class object, edit data, and write back to json file
 
     #Step 1) Load json file as dict:
-    example_restaurant_json_path = "../restaurant_json/resaurant_id_1_in_n_out.json"
+    example_restaurant_json_path = data_dir + "restaurant_json/resaurant_id_1_in_n_out.json"
     data_as_dict = load_json_file_as_dict(example_restaurant_json_path)
 
     #Step 2) Create restraunt object and read in dict:
@@ -43,14 +45,14 @@ def run_eample_two():
     #Step 4) Write restraunt to json file:
     sample_restaurant_dict = sample_restaurant.convert_to_dict()
 
-    example_restraunt_json_output_path = "../restaurant_json/sample_restaurant_output.json"
+    example_restraunt_json_output_path = data_dir + "restaurant_json/sample_restaurant_output.json"
     write_dict_to_json_file(sample_restaurant_dict, example_restraunt_json_output_path)
 
 def run_eample_three():
     #example 3: load json file of order as order class object, edit data, and write back to json file
 
     #Step 1) Load json file as dict:
-    example_order_json_path = "../order_json/order_id_1.json"
+    example_order_json_path = data_dir + "order_json/order_id_1.json"
     data_as_dict = load_json_file_as_dict(example_order_json_path)
 
     #Step 2) Create restraunt object and read in dict:
@@ -62,14 +64,14 @@ def run_eample_three():
     sample_order_dict = sample_order.convert_to_dict()
     ##json_pretty_print(sample_order_dict)
 
-    example_order_json_output_path = "../order_json/sample_order_output.json"
+    example_order_json_output_path = data_dir + "order_json/sample_order_output.json"
     write_dict_to_json_file(sample_order_dict, example_order_json_output_path)
 
 def run_eample_four():
     #example 4: load json file of confirmation as confirmation class object, edit data, and write back to json file
 
     #Step 1) Load json file as dict:
-    example_confirmation_path = "../confirmation_json/order_id_1_confirmation.json"
+    example_confirmation_path = data_dir + "confirmation_json/order_id_1_confirmation.json"
     data_as_dict = load_json_file_as_dict(example_confirmation_path)
 
     #Step 2) Create restraunt object and read in dict:
@@ -81,17 +83,17 @@ def run_eample_four():
     sample_confirmation_dict = sample_confirmation.convert_to_dict()
     json_pretty_print(sample_confirmation_dict)
 
-    example_confirmation_json_output_path = "../confirmation_json/sample_confirmation_output.json"
+    example_confirmation_json_output_path = data_dir + "confirmation_json/sample_confirmation_output.json"
     write_dict_to_json_file(sample_confirmation_dict, example_confirmation_json_output_path)
 
 def run_test_location():
     #Step 1) Load restaurant:
-    example_restaurant_json_path = "../restaurant_json/resaurant_id_1_in_n_out.json"
+    example_restaurant_json_path = data_dir + "restaurant_json/resaurant_id_1_in_n_out.json"
     data_as_dict = load_json_file_as_dict(example_restaurant_json_path)
     sample_restaurant = restaurant(data_as_dict)
 
     #Step 2) Load order:
-    example_order_json_path = "../order_json/order_id_1.json"
+    example_order_json_path = data_dir + "order_json/order_id_1.json"
     data_as_dict = load_json_file_as_dict(example_order_json_path)
     sample_order = order(data_as_dict)
 
